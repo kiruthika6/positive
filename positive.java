663cc3ceb51b938249e22c98cfc65a694bcdaa22
kiruthika6 <kiruthika.k1313056@ksrce.ac.in>
2016-08-22 17:53:27 +0530

    Add files via upload

---
+++ positive.java
@@ -0,0 +1,20 @@
+package positive;
+
+public class posneg {
+
+	
+	public static void main(String[] args) {
+		int a=-5;
+		if(a>0){
+			
+			System.out.println("positive");
+		}
+		else if(a<0){
+			System.out.println("negative");
+		}
+		else{
+			System.out.println(" boundry");
+		}
+	}
+
+}

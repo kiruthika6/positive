daca1b9ec96beb62b0c1e22402d3a5d9118a65ec
kiruthika6 <kiruthika.k1313056@ksrce.ac.in>
2016-08-24 17:56:47 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,14 @@
+package alpha;
+
+public class hello {
+
+	
+	public static void main(String[] args) {
+		int n=5;
+		String r="hello ";
+		for(int i=0;i<n;i++){
+			System.out.println(r);
+		}
+	}
+
+}

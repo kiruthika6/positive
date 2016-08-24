97e6939d0d3f3f85fa52bc05f2420291782387fa
kiruthika6 <kiruthika.k1313056@ksrce.ac.in>
2016-08-24 17:48:11 +0530

    Add files via upload

---
+++ leap.java
@@ -0,0 +1,17 @@
+package alpha;
+
+public class leap {
+
+	
+	public static void main(String[] args) {
+		int a = 2016;
+		
+		if((a%4==0)||(a%10==0)){
+			System.out.println("leapyear");
+		}
+		else{
+			System.out.println("not leapyear");
+		}
+	}
+
+}

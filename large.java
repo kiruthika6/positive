45a2857c85a118694915218c88385a8cb29d5cba
kiruthika6 <kiruthika.k1313056@ksrce.ac.in>
2016-08-24 17:39:50 +0530

    Add files via upload

---
+++ large.java
@@ -0,0 +1,22 @@
+package alpha;
+
+import java.util.Scanner;
+
+public class large {
+
+	
+	public static void main(String[] args) {
+		int a = 67,b = 78,c = 45;
+		
+		if(a>b){
+			System.out.println(a);
+		}
+		else if(b>c){
+			System.out.println(b);
+		}
+		else{
+			System.out.println(c);
+		}
+	}
+
+}
